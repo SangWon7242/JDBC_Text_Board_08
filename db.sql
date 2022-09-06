a6a542a6cf9ed170e36a038e7782cd4fdc7ea5c1
SangWon7242 <gotkdqja@gmail.com>
2022-09-06 21:36:04 +0900

    테스트 데이터 db 추가

--- db.sql
+++ db.sql
@@ -38,7 +38,7 @@ loginPw = 'test2',
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 
 # 게시물 테이블에 hit 칼러 추가
-ALTER TABLE article ADD COLUMN hit INT(10) UNSIGNED NOT NULL AFTER `body`;
+alter table article ADD COLUMN hit INT(10) UNSIGNED NOT NULL AFTER `body`;
 
 # 게시물 추가
 INSERT INTO article
@@ -47,7 +47,7 @@ updateDate = NOW(),
 title = '제목1',
 `body` = '내용1',
 memberId = 1,
-hit = 0;
+hit = 5;
 
 INSERT INTO article
 SET regDate = NOW(),
@@ -55,7 +55,7 @@ updateDate = NOW(),
 title = '제목2',
 `body` = '내용2',
 memberId = 1,
-hit = 0;
+hit = 4;
 
 INSERT INTO article
 SET regDate = NOW(),
@@ -63,7 +63,7 @@ updateDate = NOW(),
 title = '제목3',
 `body` = '내용3',
 memberId = 2,
-hit = 0;
+hit = 3;
 
 INSERT INTO article
 SET regDate = NOW(),
@@ -71,4 +71,91 @@ updateDate = NOW(),
 title = '제목4',
 `body` = '내용4',
 memberId = 2,
-hit = 0;
+hit = 20;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목5',
+`body` = '내용5',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목6',
+`body` = '내용6',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목7',
+`body` = '내용7',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목8',
+`body` = '내용8',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목9',
+`body` = '내용9',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목10',
+`body` = '내용10',
+memberId = 2,
+hit = 20;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목11',
+`body` = '내용11',
+memberId = 2,
+hit = 20;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목12',
+`body` = '내용12',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목13',
+`body` = '내용13',
+memberId = 2,
+hit = 20;
+
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목14',
+`body` = '내용14',
+memberId = 2,
+hit = 20;
